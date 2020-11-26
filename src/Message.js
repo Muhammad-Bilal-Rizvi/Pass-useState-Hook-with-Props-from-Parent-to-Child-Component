caf1b0d30e10fe5fb9febeb68a1b05e025e20b55
Muhammad Bilal Rizvi <73776975+Muhammad-Bilal-Rizvi@users.noreply.github.com>
2020-11-25 23:18:57 -0800

    Initialize project using Create React App

---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Message(props) {
+
+    return(
+        <div>
+            <h1>value of counter variable is: {props.counter}</h1>
+        </div>
+    );
+}
